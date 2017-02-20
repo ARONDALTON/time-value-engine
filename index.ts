9862a1644bc27b50182d7e2bf16951858f54aef4
Kevin Sherman <kevin.r.sherman@gmail.com>
2017-02-20 06:41:58 -0500

    initial calculations

--- index.ts
+++ index.ts
@@ -10,6 +10,21 @@ export class TimeValueCashFlowMatrix {
 }
 
 // individual line that makes up the TimeValueCashFlowMatrix
-export class TimeValueEvent {
+export interface TimeValueEvent {
 
+}
+
+export interface LoanParameters {
+    amount: number;
+    interest: number;
+    term: number;
+}
+
+//monthlyPayment = amount * (periodInt / (1 - Math.pow(1 + periodInt, -(totalTerm))));
+
+
+export function PaymentOnFixedRateLoan(p: LoanParameters): number {
+    let pInt = p.interest / 12;
+    let monthlyPayment = p.amount * ( pInt / (1 - Math.pow(1 + pInt, -(p.term))));
+    return monthlyPayment;
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -20,3 +20,17 @@ describe('TimeValueCashFlowMatrix tests', () => {
 describe('TimeValueEvent tests', () => {
 
 });
+
+describe('should calculate payment on a fixed rate loan', () => {
+    it('should equal time 699.21', () => {
+        let params : Index.LoanParameters = {
+            amount: 100000,
+            interest: 0.075,
+            term: 360
+        };
+
+        let monthlyPayment = +(Index.PaymentOnFixedRateLoan(params).toFixed(2));
+        let tvAnswer = 699.21;
+        expect(monthlyPayment).toEqual(tvAnswer);
+    })
+})
